bc9a7008cc93783f5c97e51d477b39f9df951f3a
Saul Everardo Huerta Perez <Saul@Sauls-MBP.attlocal.net>
2020-05-03 18:03:06 -0500

    displaying the data inside an active modal and using semantic-ui as the css framework

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+divEl = document.querySelector("#restaurants-container");
+
 var key = "82359ff64f3766634a1dab8ede2ba7d9";
 
 var lat = "30.2672";
@@ -33,17 +35,40 @@ var displayZomato = function(data) {
     console.log(restName,restCuisine,restCost,restAggregateRating,restLocation,restImg);
 
     // create a modal to hold restaurants info
-    // var restaurantsInfoEl = document.createElement("");
-    // restaurantsInfoEl.className ="";
-    // restaurantsInfoEl.id ="";
-    // restaurantsInfoEl.textContent = restName,restCuisine,restCost,restAggregateRating,restLocation;
+    var restaurantsNameEl = document.createElement("p");
+    restaurantsNameEl.className =" ";
+    restaurantsNameEl.id = "";
+    restaurantsNameEl.textContent = restName;
+    divEl.appendChild(restaurantsNameEl);
 
-    // modalEl.appendChild(restaurantsInfoEl);
+    var restCuisineEl = document.createElement("p");
+    restCuisineEl.className =" ";
+    restCuisineEl.id = "";
+    restCuisineEl.textContent = "Cuisine: " + restCuisine;
+    divEl.appendChild(restCuisineEl);
 
-    // var imgEl = document.createElement("img");
-    // imgEl.id = "";
-    // imgEl.src = restImg;
-    // modalEl.appendChild(imgEl);
+    var restCostEl = document.createElement("p");
+    restCostEl.className =" ";
+    restCostEl.id = "";
+    restCostEl.textContent = "Average cost for two people: $" + restCost;
+    divEl.appendChild(restCostEl);
+
+    var ratingEl = document.createElement("p");
+    ratingEl.className =" ";
+    ratingEl.id = "";
+    ratingEl.textContent = "Restaurant Rating: " + restAggregateRating;
+    divEl.appendChild(ratingEl);
+
+    var restLocationEl = document.createElement("p");
+    restLocationEl.className =" ";
+    restLocationEl.id = "";
+    restLocationEl.textContent = "Address: " + restLocation;
+    divEl.appendChild(restLocationEl);
+
+    var imgEl = document.createElement("img");
+    imgEl.id = "";
+    imgEl.src = restImg;
+    divEl.appendChild(imgEl);
 
   }
   
