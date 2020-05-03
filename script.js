7fbda95f17ac017e5a978ae05fd30459cbd39382
Andy Sanchez <andysanchez726@gmail.com>
2020-05-02 21:30:54 -0500

    added load more feature to load next page of events underneath current page

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+var body = document.querySelector("body")
 var city = "Austin";
 var eventsContainerEl = document.querySelector("#events-container");
 var eventArray = []
+var page = 0
+var totalPages = null
 
 // Get data from Ticketmaster
-var ticketmaster = function (city) {
-    fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=2umjA6L6GdnhhnFn7dbnGxUBjPWr9bDf&city=Austin&sort=date,asc")
+var ticketmaster = function (city, pageNumber) {
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=2umjA6L6GdnhhnFn7dbnGxUBjPWr9bDf&city=" + city + "&sort=date,asc&page=" + pageNumber)
     .then((response) => {
         return response.json()
         .then((data) => {
@@ -23,13 +26,26 @@ var ticketmaster = function (city) {
                     .text("Date: " + eventList[i].dates.start.localDate + " | " + eventList[i]._embedded.venues[0].name + eventList[i]._embedded.venues[0].city.name + eventList[i]._embedded.venues[0].state.stateCode);
                 // append event containter to events container
                 $(eventsContainerEl).append(event);
-                
             };
-
+            // grabs page total from API and assigns it to totalPages to be used outside of function
+            totalPages = data.page.totalPages
+            return totalPages
         });
     });
 };
 
-ticketmaster();
+// load more button click
+$("#load-more").click(function() {
+    // adds 1 to page number and runs ticketmaster function with the next page number which loads underneath current page
+    page++
+    ticketmaster("Austin", page);
+    // removes the load more button on the last page
+    if(page === totalPages-1) {
+        $("#load-more").remove()
+    };
+});
+
+
+ticketmaster("Austin", 0);
 
 
